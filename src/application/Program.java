4b33ad289e341a4b551dad47dd43487931c7c9d3
thaqnos <thaisaquino1994@gmai.com>
2021-08-20 18:39:35 -0300

    adiciona atributos, classes e metodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENTS );
+		
+		System.out.println(order);
 
 	}
 
